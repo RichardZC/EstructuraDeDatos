8712caaeac5cec9b084715237f0ce96e1a383e30
Richard Zapata <richard.zapata.c@gmail.com>
2024-04-19 11:56:56 -0500

    laboratorio 2

---
+++ CursoEstructuraDatos2024/Laboratorio2/Program.cs
@@ -0,0 +1,12 @@
+﻿// programa que sume los n primeros numeros
+
+var n = 50;
+var suma = 0;
+
+for (var i = 1; i <= n; i++)
+{
+    suma = suma + i;
+}
+
+
+Console.WriteLine("El resultado es: " + suma.ToString());
